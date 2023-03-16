48efb8f8b04583881639e2f623dc60478edcd58c
Jonathan Arnett <jonarnett90@gmail.com>
2023-03-16 15:35:24 -0400

    Comment, move end of tracing

--- lib/opis.ex
+++ lib/opis.ex
@@ -5,21 +5,29 @@ defmodule Opis do
 
   defmacro analyze(do: expr) do
     quote do
+      # Start our monitor
       {:ok, var!(server)} = Opis.Server.start_link()
+
+      # Start tracing
       :erlang.trace(self(), true, [:call, tracer: var!(server)])
       :erlang.trace_pattern({:_, :_, :_}, true, [:local])
 
       unquote(expr)
 
+      # Stop tracing
+      :erlang.trace(self(), false, [:call, tracer: var!(server)])
+
+      # Ensure all traces are delivered
       :erlang.trace_delivered(var!(server))
 
       receive do
         {:trace_delivered, _, _} -> :ok
       end
 
+      # This is a bit weird, but it ensures that all traces have been processed
+      # by the server.  When the server responds to our call, we can be assured
+      # that all the trace messages have already been handled.
       :ok = GenServer.call(var!(server), :flush)
-
-      :erlang.trace(self(), false, [:call, tracer: var!(server)])
     end
   end
 end

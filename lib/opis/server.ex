81bb973610ed337958e0132c4346156ce2e52ec0
Jonathan Arnett <jonarnett90@gmail.com>
2023-03-16 15:41:11 -0400

    Add Call and State strucs

--- lib/opis/server.ex
+++ lib/opis/server.ex
@@ -1,10 +1,20 @@
 defmodule Opis.Server do
   use GenServer
 
+  defmodule Call do
+    @type t :: %__MODULE__{call: {module(), atom(), [term()]}, children: [t()], return: term()}
+    defstruct [:call, :return, children: []]
+  end
+
+  defmodule State do
+    @type t :: %__MODULE__{calls: [Call.t()], path: [term()]}
+    defstruct calls: %Call{}, path: []
+  end
+
   ## Client
 
   def start_link() do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, %State{})
   end
 
   ## Server

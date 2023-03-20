f3e5e9f4eb6fb100ed6cc8af89db1c213732ceec
Jonathan Arnett <jonarnett90@gmail.com>
2023-03-20 09:47:49 -0400

    Change macro to a function

--- lib/opis.ex
+++ lib/opis.ex
@@ -9,14 +9,12 @@ defmodule Opis do
   Returns the result of the given expression.  Use `calls/1` to get the
   generated call tree.
   """
-  defmacro analyze(do: expr) do
-    quote do
-      Opis.Server.start_tracing()
-      result = unquote(expr)
-      Opis.Server.stop_tracing()
+  def analyze(fun) do
+    Opis.Server.start_tracing()
+    result = fun.()
+    Opis.Server.stop_tracing()
 
-      result
-    end
+    result
   end
 
   @doc """
